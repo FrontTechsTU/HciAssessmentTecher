0676f3c48cb3ff32edcc4581c6e868200f780a62
Pannawat <goku.pun@hotmail.com>
2016-11-15 00:08:32 +0700

    create start screen activity

--- TeacherAssessment/app/src/main/java/com/example/maaster/teacherassessment/LoginActivity.java
+++ TeacherAssessment/app/src/main/java/com/example/maaster/teacherassessment/LoginActivity.java
@@ -1,15 +1,19 @@
 package com.example.maaster.teacherassessment;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login2);
     }
 
